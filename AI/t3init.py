2e32ea449f2ebec0d7cef55f6cad96b3cc8386fb
Velayudham Narendharan <67109518+narenv41@users.noreply.github.com>
2025-06-17 13:44:36 +0530

    Update t3init.py

--- AI/t3init.py
+++ AI/t3init.py
@@ -15,3 +15,7 @@ if __name__ == "__main__":
     prio = initialclassify(text)
     print(prio.capitalize())
 #Unable to classify which is medium priority properly through this model
+#This model is good enough for the current scenario but we need to be able to ckassify other instacnes as well .TO achieve this we need to train it with a respective buisness use case. 
+#like if suppose we want to say that for a vendor who sells a product to an customer ,issues can be raised by the customer through this .But to identify priority we need to train our model with
+#the respective dataset in accordance to the product . THus if supposed product is unable to run is labelled low and product crashed Fully should be labelled high . Or like security vulnerabilities shouls be labeled high
+#
