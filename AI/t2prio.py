b98147ca176e49d2d2faca6e0ce47c5abd902689
Velayudham Narendharan <67109518+narenv41@users.noreply.github.com>
2025-06-17 13:41:32 +0530

    Update t2prio.py

--- AI/t2prio.py
+++ AI/t2prio.py
@@ -25,3 +25,6 @@ if __name__ == "__main__":
 #we need to be able to run this faster . FOr every message i type it should run through this model which detects emotion. 
 #BUt when initially creating the ticket we need to classify if the ticket has low priority or high priority dependind upon the user's descripton(Liket total outage:High, Application completely down:HIGH, similar to that we are supposed to be able to classify all the tickets initially ,
 #to do that we need an ai model which wil be able to classify what type of priorityty, we can either use pre-trained model or train it with respective to the company)
+#Thus to optimize the application its best to run this as a backend and to invoke it only when needed ,best to use ASGI server like Uvicorn to run in the backend and integrate it too .net.
+#Currently this model is trained on noraml words associated with emotions, it should be further trained using dataseets which depict business instances of chats making it more accurate for our use case
+#
